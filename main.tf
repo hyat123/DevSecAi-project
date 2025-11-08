a42813c559d9c2fcff8cf0e26c8a4202a21f23ec
subrena <k2313833@kingston.ac.uk>
2025-11-08 19:52:29 +0000

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-
 resource "aws_s3_bucket" "terraformbuckettodaysunn" {
   bucket = "terraformbuckettodaysunn"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformbuckettodaysunn"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
